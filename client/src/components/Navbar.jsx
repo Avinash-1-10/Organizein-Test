5b19076d25c0e10f954f8279ceded57a38331489
Avinash-1-10 <avinashyede110@gmail.com>
2024-07-14 00:22:01 +0530

    add active navbar links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const Navbar = () => {
   const [user, setUser] = useState(null);
   const [showMenu, setShowMenu] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     setUser(JSON.parse(localStorage.getItem('user')));
@@ -23,6 +24,13 @@ const Navbar = () => {
       console.log(error);
     }
   };
+
+  const getLinkClass = (path) => {
+    return location.pathname === path
+      ? 'text-blue-500'
+      : 'text-gray-300 hover:text-white';
+  };
+
   return (
     <nav className='bg-gray-800 p-4'>
       <div className='container mx-auto flex justify-between items-center'>
@@ -30,19 +38,16 @@ const Navbar = () => {
           <Link to='/'>Form App</Link>
         </div>
         <div className='flex space-x-10 items-center'>
-          <Link
-            to='/'
-            className='text-white bg-green-500 px-3 py-1 rounded hover:text-white'
-          >
+          <Link to='/' className={`${getLinkClass('/')}`}>
             Create
           </Link>
-          <Link to='/my-forms' className='text-gray-300 hover:text-white'>
+          <Link to='/my-forms' className={`${getLinkClass('/my-forms')}`}>
             My Forms
           </Link>
           {user?.isAdmin && (
             <Link
               to='/admin-dashboard'
-              className='text-gray-300 hover:text-white'
+              className={`${getLinkClass('/admin-dashboard')}`}
             >
               Admin Dashboard
             </Link>

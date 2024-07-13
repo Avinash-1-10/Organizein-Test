f55a813770329cdcc9d595366ad9171657730dc8
Avinash-1-10 <avinashyede110@gmail.com>
2024-07-13 23:27:07 +0530

    add client url env

--- server/app.js
+++ server/app.js
@@ -1,21 +1,22 @@
-import express from "express";
-import { configDotenv } from "dotenv";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import connectDB from "./config/db.js";
-import errorHandler from "./middlewares/error.middleware.js";
-import userRouter from "./routes/user.routes.js";
-import formRouter from "./routes/form.routes.js";
+import express from 'express';
+import { configDotenv } from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
+import errorHandler from './middlewares/error.middleware.js';
+import userRouter from './routes/user.routes.js';
+import formRouter from './routes/form.routes.js';
 
 configDotenv();
 connectDB();
 
 const app = express();
 const port = process.env.PORT || 4000;
+const CLIENT_URL = process.env.CLIENT_URL;
 
 // Configure CORS options
 const corsOptions = {
-  origin: 'http://localhost:5173', // Allow requests from this origin
+  origin: CLIENT_URL, // Allow requests from this origin
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 };
 
@@ -24,8 +25,8 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/users", userRouter);
-app.use("/api/forms", formRouter);
+app.use('/api/users', userRouter);
+app.use('/api/forms', formRouter);
 
 // Error handling middleware
 app.use(errorHandler);

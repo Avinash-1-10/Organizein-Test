bf7588d9bad9adadda52449c6cdc1ba15331820d
Avinash-1-10 <avinashyede110@gmail.com>
2024-07-14 00:12:50 +0530

    update auth middleware

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,28 +1,29 @@
-import { CustomError } from "../utils/customError.js";
-import jwt from "jsonwebtoken";
-import verifyToken from "../utils/verifyToken.js";
-import User from "../models/user.model.js";
+import { CustomError } from '../utils/customError.js';
+import jwt from 'jsonwebtoken';
+import verifyToken from '../utils/verifyToken.js';
+import User from '../models/user.model.js';
 
 const authMiddleware = async (req, res, next) => {
   try {
-    const token = req.cookies.auth_token;
-    
+    const token =
+      req.cookies.auth_token || req.headers.auth_token.split(' ')[1];
+
     if (!token) {
-      throw new CustomError("Authentication required", 401);
+      throw new CustomError('Authentication required', 401);
     }
 
     // Verify the token using a separate async function
     const decoded = await verifyToken(token);
     const userId = decoded.id;
-    
+
     if (!userId) {
-      throw new CustomError("Authentication required - invalid token", 401);
+      throw new CustomError('Authentication required - invalid token', 401);
     }
 
     const user = await User.findById(userId);
-    
+
     if (!user) {
-      throw new CustomError("Authentication required - user not found", 401);
+      throw new CustomError('Authentication required - user not found', 401);
     }
 
     req.user = user;
@@ -35,18 +36,18 @@ const authMiddleware = async (req, res, next) => {
 const handleAuthError = (error, res) => {
   // Handle JWT errors
   if (error instanceof jwt.TokenExpiredError) {
-    console.error("JWT expired:", error.message); // Log for debugging
-    return res.status(401).json( CustomError("Token expired", 401));
-  } 
-  
+    console.error('JWT expired:', error.message); // Log for debugging
+    return res.status(401).json(CustomError('Token expired', 401));
+  }
+
   if (error instanceof jwt.JsonWebTokenError) {
-    console.error("JWT verification error:", error.message); // Log for debugging
-    return res.status(401).json(new CustomError("Invalid token", 401));
-  } 
-  
+    console.error('JWT verification error:', error.message); // Log for debugging
+    return res.status(401).json(new CustomError('Invalid token', 401));
+  }
+
   // Handle other errors
-  console.error("Internal server error:", error.message); // Log for debugging
-  return res.status(500).json(new CustomError("Internal server error", 500));
+  console.error('Internal server error:', error.message); // Log for debugging
+  return res.status(500).json(new CustomError('Internal server error', 500));
 };
 
 export default authMiddleware;

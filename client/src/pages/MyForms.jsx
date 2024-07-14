bbcdc14db28b22a0e5ac2d7280db05e8fee412ff
Avinash-1-10 <avinashyede110@gmail.com>
2024-07-14 10:39:43 +0530

    add loading for pages

--- client/src/pages/MyForms.jsx
+++ client/src/pages/MyForms.jsx
@@ -7,19 +7,39 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 const MyForms = () => {
   const [forms, setForms] = useState([]);
   const [reload, setReload] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const getForms = async () => {
     try {
+      setLoading(true);
       const { data } = await axios.get(`${BACKEND_URL}/api/forms/user`);
       setForms(data.data);
     } catch (error) {
       console.log(error);
+    }finally{
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     getForms();
   }, [reload]);
+
+  if (loading) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <div className='w-16 h-16 border-4 border-dashed rounded-full animate-spin border-blue-500'></div>
+      </div>
+    );
+  }
+
+  if (forms.length === 0) {
+    return (
+      <div className='container mx-auto p-6'>
+        <h1 className='text-3xl font-bold mb-6 text-center'>No Forms Found</h1>
+      </div>
+    );
+  }
 
   return (
     <>
